8f942a78c5996387ae34884e49603c48b7f960e7
FAROOQ KHAN <farooq.khan@caci.com>
2024-08-14 15:32:29 -0400

    Added Flash Card class

---
+++ StudySmart/src/main/java/com/example/StudySmart/FlashCard.java
@@ -0,0 +1,53 @@
+package com.example.StudySmart;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table
+public class FlashCard {
+    @Id
+    @SequenceGenerator(
+            name = "FlashCard_sequence",
+            sequenceName = "FlashCard_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
+    private long id;
+
+    private String question;
+
+    private String answer;
+
+    public FlashCard() {
+    }
+
+    public FlashCard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+}
