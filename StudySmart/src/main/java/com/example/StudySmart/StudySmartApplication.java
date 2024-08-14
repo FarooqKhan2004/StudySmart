d1f2ef9a46c4788fa1dc1454c461390aeb20db2c
FAROOQ KHAN <farooq.khan@caci.com>
2024-08-14 15:12:07 -0400

    Initialize Spring Boot backend

---
+++ StudySmart/src/main/java/com/example/StudySmart/StudySmartApplication.java
@@ -0,0 +1,13 @@
+package com.example.StudySmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudySmartApplication.class, args);
+	}
+
+}
---
+++ StudySmart/src/test/java/com/example/StudySmart/StudySmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudySmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
